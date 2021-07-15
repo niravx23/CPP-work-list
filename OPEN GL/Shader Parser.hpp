d66b60148b18c683bc9e6ccc847647031aa3b23c
niravx23 <80905362+niravx23@users.noreply.github.com>
2021-07-15 17:21:33 +0530

    for creating a program from only one GLSL file

--- OPEN GL/Shader Parser.hpp
+++ OPEN GL/Shader Parser.hpp
@@ -5,6 +5,18 @@
 #include <string>
 
 using namespace std; 
+//----------------------------------UPDATE-------------------------------------------\\
+/*
+UNDER NAMESPACE program 
+new function added to make a program providing only one glsl file location having all shaders in it . The example of which can be found on any glsl file on the rep .
+
+eg :-
+[in main func]
+
+unsigned int prg  =  program::make_program("location of your glsl file");
+
+*/
+
 
 /*          ////////!\\\\\\\\\
  ----- > THIS PARSER ALLOWS YOU TO WRITE ALL TYPES OF SHADER IN SAME FILE .
@@ -15,6 +27,8 @@ using namespace std;
        2) CREATE STRINGS FOR DIFFERENT SHADERS .
        3) USE GET_SHADERNAME_SHADER() TO ASSING THE SHADERS ITS SOURCE .
 */
+
+
 class ShaderParser{
   private :   ifstream parse  ;
        enum modes {
@@ -62,3 +76,29 @@ class ShaderParser{
 
      return FragmentShaderSource ;
    }
+
+namespace program{
+unsigned int make_program(string main1)
+{
+ShaderParser in1(main1); 
+
+string a  =in1.GET_VERTEX_SHADER();
+string b  =  in1.GET_FRAGMENT_SHADER();
+
+const char* VertexShaderSource = a.c_str();
+const char* FragmentShaderSource =  b.c_str();
+    unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &VertexShaderSource, NULL);
+    glCompileShader(vertex);
+
+    unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &FragmentShaderSource, NULL);
+    glCompileShader(fragment);
+
+    unsigned int prg = glCreateProgram();
+    glAttachShader(prg, vertex);
+    glAttachShader(prg, fragment);
+    glLinkProgram(prg);
+    return prg;
+}
+}
